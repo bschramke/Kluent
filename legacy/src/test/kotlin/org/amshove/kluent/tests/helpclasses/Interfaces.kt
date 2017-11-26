4100f6a39efadfb767d7ebc9be166b256cf17c62
Björn Schramke <bjoern@schramke-online.de>
2017-11-26 22:53:15 +0100

    move current code into legacy module and create new core module

---
+++ legacy/src/test/kotlin/org/amshove/kluent/tests/helpclasses/Interfaces.kt
@@ -0,0 +1,12 @@
+package org.amshove.kluent.tests.helpclasses
+
+
+interface Database {
+    fun getPerson(): Person
+    fun getPerson(id: Int): Person
+}
+
+interface Shape {
+    fun getArea(): Double
+}
+
